6578eebaad0183c92f019054067d7702d5e604e2
kennethgallo <61844301+kennethgallo@users.noreply.github.com>
2023-12-05 16:28:46 -0500

    fixing some code errors and initial commit

--- main.py
+++ main.py
@@ -10,74 +10,50 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API_KEY = "TL3S50KODF1BTNJH"
 NEWS_API_KEY = "e269a25c300b4b28b5daeef2c53e11ba"
 TWILIO_SID = "ACe213dccca9b789431bbbcced7be811fd"
-TWILION_AUTH_TOKEN = "1fbb95d3187e9fb4e148d43efccc848d"
+TWILIO_AUTH_TOKEN = "1fbb95d3187e9fb4e148d43efccc848d"  # Corrected variable name
 
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
+# Get stock data
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
     "apikey": STOCK_API_KEY,
 }
-
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
 data = response.json()["Time Series (Daily)"]
-data_list = [value for (key, value) in data.items()]
+
+# Sort the data by date to ensure correct order
+sorted_data = sorted(data.items(), key=lambda x: x[0], reverse=True)
+data_list = [value for (key, value) in sorted_data]
+
+# Get yesterday's and the day before yesterday's closing stock price
 yesterday_data = data_list[0]
-yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
-
-
-
-#Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
+
+yesterday_closing_price = yesterday_data["4. close"]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
-print(day_before_yesterday_closing_price)
 
-#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+# Calculate the percentage difference
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+diff_percent = (difference / float(day_before_yesterday_closing_price)) * 100
 
-#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
-print(diff_percent)
-
-#If percentage is greater than 5 then print("Get News").
-
-if diff_percent > 0:
+# Check if the difference is greater than 5%
+if diff_percent > 5:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
     }
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()["articles"]
-
-#Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles[:3]
-    print(three_articles)
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
-    #"Headline: {article title}. \nBrief: {article description}"
+    # Format articles for SMS
+    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 
-
-#Create a new list of the first 3 article's headline and description using list comprehension.
-    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in
-                          three_articles]
-
-#TODO 9. - Send each article as a separate message via Twilio.
-    client = Client(TWILIO_SID, TWILION_AUTH_TOKEN)
-
+    # Send SMS for each article
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
     for article in formatted_articles:
         message = client.messages.create(
             body=article,
-            from="+18445360661",
-            to="+17044419451",
+            from_="+18445360661",  # Replace with your Twilio number
+            to="+17044412275",    # Replace with your phone number
         )
-
-
-
